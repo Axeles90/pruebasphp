46a3b2b32322f853857bfa3709b275fd79499384
Axeles90 <pablopolliniyepes@gmail.com>
2024-11-09 20:36:07 +0100

    primeras pruebas con php

---
+++ practica3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pruebasphp</title>
+</head>
+<body>
+    <h1>Hola</h1>
+    <?php
+        $entero = 0;
+        $decimal = 0.5;
+        $cadena = "hola";
+        $cadena2 = "0256";
+        $nulo = null;  
+        
+        echo 'la conversion a boolean de la variable $entero es '.(boolean)$entero;
+        echo "<br>";
+        echo 'la conversion a boolean de la variable $entero es '.(boolean)$decimal;
+        echo "<br>";
+        echo 'la conversion a boolean de la variable $entero es '.(boolean)$cadena;
+        echo "<br>";
+        echo 'la conversion a boolean de la variable $entero es '.(boolean)$cadena2;
+        echo "<br>";
+        echo 'la conversion a boolean de la variable $entero es '.(boolean)$nulo;
+        echo "<br>"; 
+    ?>
+</body>
+</html>
